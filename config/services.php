cbe215ac46c411febee68f29c295efe3b8e714a7
murri06 <ruslanliakhovets@outlook.com>
2024-02-03 20:08:57 +0200

    secret revoke

--- config/services.php
+++ config/services.php
@@ -32,8 +32,8 @@ return [
     ],
 
     'facebook' => [
-        'client_id' => '813438547216650',
-        'client_secret' => '87474ad176c2ba9437bfd7b0cd8d5dc7',
+        'client_id' => '***',
+        'client_secret' => '***',
         'redirect' => 'http://localhost/callback/facebook',
     ]
 
